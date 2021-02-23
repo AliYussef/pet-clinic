6c94a801526502862cd6e33242353ebb5538ca7d
aliyussef <a.yussef@outlook.com>
2021-02-23 21:14:08 +0100

    Initial commit

---
+++ src/main/java/ay/springframework/petclinic/PetClinicApplication.java
@@ -0,0 +1,13 @@
+package ay.springframework.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplication.class, args);
+    }
+
+}
