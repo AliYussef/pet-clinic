d3eb40dd9db263c1af424e458722392663ab4687
aliyussef <a.yussef@outlook.com>
2021-03-04 23:53:23 +0100

    Added SpecialityService

---
+++ pet-clinic-data/src/main/java/ay/springframework/petclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package ay.springframework.petclinic.services;
+
+import ay.springframework.petclinic.model.Speciality;
+
+/**
+ * Created by aliyussef on 04/03/2021
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/ay/springframework/petclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,38 @@
+package ay.springframework.petclinic.services.map;
+
+import ay.springframework.petclinic.model.Speciality;
+import ay.springframework.petclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by aliyussef on 04/03/2021
+ */
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
