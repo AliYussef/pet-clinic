9341e1d1bbf23ed0631fc721ff43252d6180d7b0
aliyussef <a.yussef@outlook.com>
2021-03-04 23:15:09 +0100

    Added PetTypeService

---
+++ pet-clinic-data/src/main/java/ay/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package ay.springframework.petclinic.services;
+
+import ay.springframework.petclinic.model.PetType;
+
+/**
+ * Created by aliyussef on 04/03/2021
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
