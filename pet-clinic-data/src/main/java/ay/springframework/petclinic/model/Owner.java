fee6637a778177e8ccc33286c789b8feef7f268d
aliyussef <a.yussef@outlook.com>
2021-03-04 23:04:18 +0100

    Added Visit model and updated owner with Pet.

--- pet-clinic-data/src/main/java/ay/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/ay/springframework/petclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package ay.springframework.petclinic.model;
 
+import java.util.Set;
+
 /**
  * @author aliyussef
  */
-public class Owner extends Person{
+public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
