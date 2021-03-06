7dd88050c86634200dcc11555595d6e3ef485cea
aliyussef <a.yussef@outlook.com>
2021-03-06 23:46:18 +0100

    Updated Visit to use JPA.

--- pet-clinic-data/src/main/java/ay/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/ay/springframework/petclinic/model/Visit.java
@@ -1,14 +1,23 @@
 package ay.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by aliyussef on 04/03/2021
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
