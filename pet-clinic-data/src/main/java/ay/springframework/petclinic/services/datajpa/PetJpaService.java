79ad4184fd61ccec947515cdba2fd44026478bc9
aliyussef <a.yussef@outlook.com>
2021-03-07 00:31:04 +0100

    Added data jpa service for pets.

---
+++ pet-clinic-data/src/main/java/ay/springframework/petclinic/services/datajpa/PetJpaService.java
@@ -0,0 +1,51 @@
+package ay.springframework.petclinic.services.datajpa;
+
+import ay.springframework.petclinic.model.Pet;
+import ay.springframework.petclinic.repositories.PetRepository;
+import ay.springframework.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by aliyussef on 07/03/2021
+ */
+@Service
+@Profile("springdatajpa")
+public class PetJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
