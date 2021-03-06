a13f9f8daaf6474997697f138c6608a86804916d
aliyussef <a.yussef@outlook.com>
2021-03-06 23:18:33 +0100

    Updated BaseEntity with JPA annotations.

--- pet-clinic-data/src/main/java/ay/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/ay/springframework/petclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package ay.springframework.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * @author aliyussef
  */
+@MappedSuperclass //this will tells jpa that we do not need this class to be mapped to the db
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
