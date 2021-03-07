25869425ced2b524baafb4629f91f88c164e0676
aliyussef <a.yussef@outlook.com>
2021-03-07 12:44:33 +0100

    Added Map impl for visit service.

---
+++ pet-clinic-data/src/main/java/ay/springframework/petclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package ay.springframework.petclinic.services;
+
+import ay.springframework.petclinic.model.Visit;
+
+
+/**
+ * Created by aliyussef on 07/03/2021
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
