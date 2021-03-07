77d80cecf0f65e08bbbed3c1d785fb92ec8e663a
aliyussef <a.yussef@outlook.com>
2021-03-07 13:40:43 +0100

    Added project lombok annotations.

--- pet-clinic-data/src/main/java/ay/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/ay/springframework/petclinic/model/BaseEntity.java
@@ -1,5 +1,8 @@
 package ay.springframework.petclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,18 +12,12 @@ import java.io.Serializable;
 /**
  * @author aliyussef
  */
+@Getter
+@Setter
 @MappedSuperclass //this will tells jpa that we do not need this class to be mapped to the db
 public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
