06c83e5a7d2353478da0be2041b1c7375092e95f
aliyussef <a.yussef@outlook.com>
2021-03-10 20:49:00 +0100

    Added unit tests for ownerservicemap.

--- pet-clinic-data/src/main/java/ay/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/ay/springframework/petclinic/model/BaseEntity.java
@@ -1,6 +1,8 @@
 package ay.springframework.petclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
@@ -14,6 +16,8 @@ import java.io.Serializable;
  */
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass //this will tells jpa that we do not need this class to be mapped to the db
 public class BaseEntity implements Serializable {
 
