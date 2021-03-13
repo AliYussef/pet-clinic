ec665f0f957fcafa9c19fdd0b55174a2c64f8206
aliyussef <a.yussef@outlook.com>
2021-03-13 22:04:53 +0100

    Added owners details.

--- pet-clinic-web/src/main/java/ay/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ay/springframework/petclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package ay.springframework.petclinic.controllers;
 import ay.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by aliyussef on 25/02/2021
@@ -29,5 +32,13 @@ public class OwnerController {
     @RequestMapping("find")
     public String findOwners(Model model) {
         return "NotImplemented";
+    }
+
+    @GetMapping("{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+        modelAndView.addObject(ownerService.findById(ownerId));
+
+        return modelAndView;
     }
 }
