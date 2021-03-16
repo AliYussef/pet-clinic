75d8fd1fa93acadbf2a5977c681c364e23f7e0f9
aliyussef <a.yussef@outlook.com>
2021-03-17 00:15:45 +0100

    Added vet list.

--- pet-clinic-web/src/main/java/ay/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ay/springframework/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package ay.springframework.petclinic.controllers;
 
+import ay.springframework.petclinic.model.Vet;
 import ay.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * Created by aliyussef on 25/02/2021
@@ -17,11 +22,18 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+
+        return vetService.findAll();
     }
 }
